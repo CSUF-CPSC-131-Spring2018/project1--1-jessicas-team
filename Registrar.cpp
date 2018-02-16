af21b8503325c7ea57c55de8000556538b4c846e
vargasjm <33645787+vargasjm@users.noreply.github.com>
2018-02-15 17:19:26 -0800

    Update Registrar.cpp

--- Registrar.cpp
+++ Registrar.cpp
@@ -30,9 +30,21 @@ void Registrar::readTextfile(string filename) {
 // getStudent must throw an exception if cwid is invalid
 Student& Registrar::getStudent(string cwid) const {
 	// TO BE COMPLETED
+	int index=0;
+	while(index<size){		//add size in default set 0
+		if(student[i].getCWID==CWID){
+			return student[i];
+		}
+		else {
+			index++;
+		}
+	}
+	throw invalid_arguement;
 }
 
 // process a line from the text file
+//if student exists, it updates and adds course grade to their info 
+//if student does not exist, then adds student to student array and updates size
 void Registrar::addLine(string courseName, string cwid, char grade) {
 	// TO BE COMPLETED
 
