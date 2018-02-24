14e33a44a6f8df33c4cd0b5995eec9a56334d6a6
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2018-02-24 14:51:51 -0800

    Update Registrar.h

--- Registrar.h
+++ Registrar.h
@@ -7,7 +7,8 @@ using namespace std;
 
 class Registrar {
 public:
-
+	Registrar(); //Constructor // TO BE COMPLETED
+	~Registrar(); //Destructor // TO BE COMPLETED
 	void readTextfile(string filename); // Load information from a text file with the given filename: THIS IS COMPLETE
 	void addLine(string courseName, string cwid, char grade); // process a line from the text file
 	Student& getStudent(string cwid) const; // return the Student object corresponding to a given CWID
@@ -18,5 +19,10 @@ private:
 	// Add private member variables for your class along with any 
 	// other variables required to implement the public member functions
 	// TO BE COMPLETED
-
+	
+	string coursName, cwid; //possibly unnecessary.**
+	char grade; //possibly unnecessary.**
+	int size = 0; //Number of Students in the class. //Can probably set to 0 in the .cpp instead of .h 
+	const int max = 50000; //possibly unnecessary.**
+	student *indiv; //keep this
 };
