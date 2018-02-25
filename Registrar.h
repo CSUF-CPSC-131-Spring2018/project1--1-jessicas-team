6ee49acdd5573a8729c16aca8a9e268e796fdb65
vargasjm <33645787+vargasjm@users.noreply.github.com>
2018-02-24 17:33:25 -0800

    Update Registrar.h

--- Registrar.h
+++ Registrar.h
@@ -7,22 +7,28 @@ using namespace std;
 
 class Registrar {
 public:
-	Registrar(); //Constructor // TO BE COMPLETED
-	~Registrar(); //Destructor // TO BE COMPLETED
+
+	Registrar();
+	//~Registrar();
 	void readTextfile(string filename); // Load information from a text file with the given filename: THIS IS COMPLETE
 	void addLine(string courseName, string cwid, char grade); // process a line from the text file
 	Student& getStudent(string cwid) const; // return the Student object corresponding to a given CWID
-	// getStudent must throw an exception if cwid is invalid
-	// add constructors, destructors, assignment operators if needed
-
+											// getStudent must throw an exception if cwid is invalid
+											// add constructors, destructors, assignment operators if needed
+	Registrar & operator=(const Registrar & reg);
+	
+	Registrar(const Registrar &reg);
+	
 private:
 	// Add private member variables for your class along with any 
 	// other variables required to implement the public member functions
 	// TO BE COMPLETED
 	
-	string coursName, cwid; //possibly unnecessary.
-	char grade; //possibly unnecessary.
-	int size = 0; //Number of Students in the class. //Can probably set to 0 in the .cpp instead of .h 
-	const int max = 50000; //possibly unnecessary.**
-	student *indiv; //keep this
+	string courseName, cwid;
+	string filename;
+	char grade;
+	int size=0;
+	const int max=50000;
+	Student* indiv;
+	
 };
