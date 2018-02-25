b1a3986e2809273cebfa66ab0948e7cc1861a897
TommyH409 <36341132+TommyH409@users.noreply.github.com>
2018-02-24 16:52:09 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -76,7 +76,7 @@ double Student::getGPA() {
 void Student::printTranscript() { 
 	// TO BE COMPLETED.
 	cout<< "TRANSCRIPT FOR CWID= " << cwid <<endl; //Displays CWID of student.
-	for (int i=0; i <= courseNum; i++) //Loops through the number of courses.
+	for (int i=0; i < courseNum; i++) //Loops through the number of courses.
 	{
 		cout<< courseName[i] << " " << Cgrade[i] << endl; //Displays name of course and corresponding grade.
 	}
