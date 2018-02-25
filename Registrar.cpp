38153ff0c6308e0ab0b0b52b831331356591e344
vargasjm <33645787+vargasjm@users.noreply.github.com>
2018-02-24 17:31:21 -0800

    Update Registrar.cpp

--- Registrar.cpp
+++ Registrar.cpp
@@ -3,8 +3,24 @@
 #include <fstream>
 #include <stdexcept>
 #include "Registrar.h"
+#include "Student.h"
 
 using namespace std;
+
+Registrar::Registrar()
+{
+	indiv = new Student[max];
+	size = 0;
+	
+}
+//
+//Registrar::~Registrar()
+//{
+//	/*delete[] indiv;
+//	indiv = NULL;
+//*/
+//}
+
 
 // Load information from a text file with the given filename
 // THIS FUNCTION IS COMPLETE
@@ -17,7 +33,7 @@ void Registrar::readTextfile(string filename) {
 		string cwid;
 		char grade;
 		while (myfile >> courseName >> cwid >> grade) {
-			// cout << cwid << " " << grade << endl;
+			 /*cout << cwid << " " << grade << endl;*/
 			addLine(courseName, cwid, grade);
 		}
 		myfile.close();
@@ -26,23 +42,53 @@ void Registrar::readTextfile(string filename) {
 		throw invalid_argument("Could not open file " + filename);
 }
 
-// return Student object corresponding to a given CWID
-// getStudent must throw an exception if cwid is invalid
+ /*return Student object corresponding to a given CWID
+ getStudent must throw an exception if cwid is invalid*/
 Student& Registrar::getStudent(string cwid) const {
 	// TO BE COMPLETED
-	//DELETE LATER
-	//student *ptr;
-	//ptr = head;
-	//bool found;
-	//found = false;
-	//while ((ptr != NULL) && (found == false)){
-	//if(ptr-> getCWID() == tcwid){
-	//found = true; }
-	//else {
-	//throw invalid_argument ("Invalid CWID"); }
-	//if (found == true) {
-	//return *ptr } }
+	int index = 0;
+	
+	while (index < size)
+	{		//add size in default set 0
+		
+		if (indiv[index].getCWID() == cwid) {
+			return indiv[index];	//return indiv[i]
+		}
+		else {
+			index++;
+		}
+	}
+	throw invalid_argument("Invalid cwid");
 }
+
+
+Registrar & Registrar::operator=(const Registrar & reg)
+{
+	courseName = reg.courseName;
+	cwid = reg.cwid;
+	filename = reg.filename;
+	grade = reg.grade;
+	size = reg.size;
+	for (int i = 0; i < max; i++) {
+		indiv[i] = reg.indiv[i];
+	}
+	return *this;
+}
+
+Registrar::Registrar(const Registrar & reg) : max(50000)
+{
+	indiv = new Student[max];
+	 courseName= reg.courseName;
+	 cwid = reg.cwid;
+	 filename = reg.filename;
+	 grade = reg.grade;
+	 size = reg.size;
+	 for (int i = 0; i < max; i++) {
+		 indiv[i] = reg.indiv[i];
+	 }
+}
+
+
 
 // process a line from the text file
 //if student exists, it updates and adds course grade to their info 
@@ -50,33 +96,21 @@ Student& Registrar::getStudent(string cwid) const {
 void Registrar::addLine(string courseName, string cwid, char grade) {
 	// TO BE COMPLETED
 	try {
-		//updating an already existing student.
-		student &s = getStudent(cwid);
+		//We are updating an already exisiting student
+		Student &s = getStudent(cwid);
 		s.addCourseGrade(courseName, grade);
+		/*s.courseNum++;*/
 	}
-	catch (excepion &e)
+	catch (exception &e)
 	{
-		//DELETE LATER
-		//student *t= new student(cwid);
-		//t-> setNext(head);
-		//head = t;
-		//size++;
-		//t-> addCourseGrade(courseName, grade); 
-		indiv[size] = student(cwid);
+		indiv[size] = Student(cwid);
+		
 		indiv[size].addCourseGrade(courseName, grade);
+		/*indiv[size].courseNum++;*/
 		size++;
+		
+
 	}
-}
-
-Registrar::Registrar() //default constructor.
-	// TO BE COMPLETED
-{
-	//size = 0;
-	//head = nullptr;
-}
-
-Registrar::~Registrar() //destructor.
-	// TO BE COMPLETED
-{
+	
 	
 }
