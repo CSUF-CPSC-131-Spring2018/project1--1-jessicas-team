9e28cad93b9dc52c0be8bad9bf82504c21568943
vargasjm <33645787+vargasjm@users.noreply.github.com>
2018-02-24 17:34:07 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,69 +1,62 @@
 #include "Student.h"
 
 #include <iostream>
-
+#include <iomanip>
 Student::Student() {
-	//COMPLETED
-	cwid = " "; //assigns student blank cwid.
-	courseNum = 0; //sets amount of courses to 0;
-	// next = nullptr; //DELETE LATER
+	cwid = "";
+	courseNum = 0;
 }
 
 Student::Student(const string &tcwid) {
-	//COMPLETED
-	cwid = tcwid; //references cwid to a variable. 
+	cwid = tcwid;
 	courseNum = 0;
-	// next -> nullptr; //DELETE LATER
 }
 
-
-string Student::getCWID() { 
-	//COMPLETED.
+string Student::getCWID() {
 	return cwid;
 }
 
-void Student::addCourseGrade (const string &courseName, char grade) {
-	//COMPLETED
-	Tcourse[courseNum]= courseName; //references courseName.
-	Cgrade[courseNum]= grade;
-	courseNum++; //increases course grade amount by 1.
+void Student::addCourseGrade(const string &courseName, char grade) {
+	Tcourse[courseNum] = courseName;
+	Cgrade[courseNum] = grade;
+	courseNum ++;
 }
 
- 
-double Student::getGPA() { 
-	//COMPLETED.
-	//gpa is calculated from adding all values and dividing by the number of courses.
-	char grade;
-	double gpa;
-	double sum = 0, total = 0;
+double Student::getGPA() {
+	char grade;								//grades associated with CWID, need to gather all?
+	double gpa = 0, sum = 0, total=0;
+
 	
-	for (int i=0; i < courseNum; i++)
-	{
-		grade = Cgrade[i]; //switch statement to apply a value to a grade.
-		switch grade {
-			case 'A':
-				sum = 4;
-				break;
-			case 'B':
-				sum = 3;
-				break;
-			case 'C':
-				sum = 2;
-				break;
-			case 'D':
-				sum = 1;
-				break;
-			case 'F':
-				sum = 0;
-				break;
-			default:
-				sum = 0;
+	for (int i = 0; i < courseNum; i++) {
+		grade = Cgrade[i];
+		switch (grade) {
+		case 'A': 
+			sum = 4;
+			break;
+		case 'B': 
+			sum = 3;
+			break;
+		case 'C': 
+			sum = 2;
+			break;
+		case 'D': 
+			sum = 1;
+			break;
+		case 'F':
+			sum = 0;
+			break;
+		default:
+			sum = 0;
 		}
-		total = total + sum; //adds up all the values.
+		total=total+sum;
 	}
-	gpa = total / courseNum;
-	return gpa;//Returns GPA.
+
+	gpa=total / courseNum;
+	
+	return gpa;
 }
+
+
 
 // print transcript in this (sample) format:
 // TRANSCRIPT FOR CWID=279750343
@@ -71,20 +64,12 @@ double Student::getGPA() {
 // CS 253		B
 // CS 131		B
 // GPA = 2.6667
+void Student::printTranscript() {
+	// TO BE COMPLETED
 
-
-void Student::printTranscript() { 
-	// TO BE COMPLETED.
-	cout<< "TRANSCRIPT FOR CWID= " << cwid <<endl; //Displays CWID of student.
-	for (int i=0; i < courseNum; i++) //Loops through the number of courses.
-	{
-		cout<< courseName[i] << " " << Cgrade[i] << endl; //Displays name of course and corresponding grade.
-	}
-	cout<< "GPA= " << getGPA() << endl;//Displays GPA of Current Student.
+	cout << "\nTRANSCRIPT FOR CWID = " << cwid << endl;
+	for (int i = 0; i < (courseNum); i++){
+		cout <<setw(10)<<left<< Tcourse[i] <<setw(10)<<right<< Cgrade[i] << endl;
+		}
+		cout<<"GPA="<< getGPA() << endl;
 }
-
-//Student *getNext(){ //DELETE LATER
-//return next; }
-
-//void Student::getNext(Student *tnext){ //DELETE LATER
-//next= tnext; }
